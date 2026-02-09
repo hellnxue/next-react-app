613e82be8cc4f266f7394d7aefecea31492fd325
hellnpig <h1032755304@163.com>
2026-02-09 17:35:31 +0800

    错误处理

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,8 @@
 import { z } from 'zod';
 import postgres from 'postgres';
 import console from 'console';
- 
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
   id: z.string(),
@@ -14,6 +15,8 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
@@ -28,12 +31,52 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+      
+      await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // We'll also log the error to the console for now
+      console.error(error);
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
+
 
       console.log('Invoice created!');
-      
+      // 调用 revalidatePath 以清除客户端缓存并发出新的服务器请求。
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
 
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  //revalidatePath 函数用于清除客户端缓存，并重新获取最新的数据。当调用此函数时，Next.js 会自动处理缓存失效和重新渲染的问题。
+  //在这个例子中，它将会触发新的服务器请求，并重新渲染表格。
+  revalidatePath('/dashboard/invoices');
 }
